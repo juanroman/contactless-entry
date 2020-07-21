cea3f343f3c5cab60b48301f1d21f68bf3849d6a
Juan Roman Escamilla <juanroman@hotmail.com>
2020-07-21 12:51:02 -0500

    Fixing some code smells

--- src/ContactlessEntry.Cloud/Controllers/FaceRecognitionController.cs
+++ src/ContactlessEntry.Cloud/Controllers/FaceRecognitionController.cs
@@ -8,7 +8,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Mime;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace ContactlessEntry.Cloud.Controllers
