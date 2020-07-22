7276572b31afed403a7d39f46c390d0f3bf913ad
Juan Roman Escamilla <juanroman@hotmail.com>
2020-07-22 18:19:25 -0500

    Adding Authenticate Integration Tests

---
+++ tests/ContactlessEntry.Cloud.UnitTests/Integration/AuthenticateTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ContactlessEntry.Cloud.UnitTests.Integration
+{
+    public class AuthenticateTests : IntegrationTestBase
+    {
+        public AuthenticateTests(ITestOutputHelper outputHelper, WebApplicationFactory<Startup> factory) : base(outputHelper, factory)
+        {
+        }
+
+        [Fact]
+        public async Task AuthenticatePost_WithCorrectInput_ReturnsOk()
+        {
+            var dto = new
+            {
+                apiKey = $"{Guid.NewGuid()}",
+                apiSecret = $"{Guid.NewGuid()}"
+            };
+
+            var response = await RequestService.PostAsync<object, AuthenticateResponse>("/v1/authenticate", dto);
+            response.Should().NotBeNull();
+            response.Token.Should().NotBeNullOrWhiteSpace();
+        }
+
+        class AuthenticateResponse
+        {
+            public string Token { get; set; }
+        }
+    }
+}
---
+++ tests/ContactlessEntry.Cloud.UnitTests/Integration/IntegrationTestBase.cs
@@ -0,0 +1,51 @@
+﻿using ContactlessEntry.Cloud.Configuration;
+using ContactlessEntry.Cloud.Models.DataTransfer;
+using ContactlessEntry.Cloud.UnitTests.Utilities;
+using Divergic.Logging.Xunit;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ContactlessEntry.Cloud.UnitTests.Integration
+{
+    public class IntegrationTestBase : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        public IntegrationTestBase(
+            ITestOutputHelper outputHelper,
+            WebApplicationFactory<Startup> factory)
+        {
+            var loggerFactory = LogFactory.Create(outputHelper);
+            loggerFactory.AddProvider(new TestOutputLoggerProvider(outputHelper));
+
+            var httpClient = factory.CreateClient();
+            RequestService = new TestRequestService(httpClient, loggerFactory);
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile("appsettings.Development.json", optional: true)
+                .Build();
+
+            MicroserviceSettings = configuration.GetSection(nameof(MicroserviceSettings)).Get<MicroserviceSettings>();
+        }
+
+        protected MicroserviceSettings MicroserviceSettings { get; private set; }
+
+        protected TestRequestService RequestService { get; private set; }
+
+        protected async Task<string> GetJwtTokenAsync()
+        {
+            var credentials = await RequestService.PostAsync<ApiCredentialsDto, AuthenticateResponseDto>("/v1/authenticate", new ApiCredentialsDto
+            {
+                ApiKey = $"{Guid.NewGuid()}",
+                ApiSecret = $"{Guid.NewGuid()}"
+            });
+
+            return credentials?.Token;
+        }
+    }
+}
