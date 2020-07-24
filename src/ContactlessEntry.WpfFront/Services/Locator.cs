6da7eccc8060b52449276622f0e1b373cc3222e7
Juan Roman Escamilla <juanroman@hotmail.com>
2020-07-24 01:47:19 -0500

    Beginning work on WPF client

---
+++ src/ContactlessEntry.WpfFront/Services/Locator.cs
@@ -0,0 +1,59 @@
+﻿using Autofac;
+using System;
+
+namespace ContactlessEntry.WpfFront.Services
+{
+    public sealed class Locator
+    {
+        private IContainer _container;
+
+        public static Locator Instance { get; } = new Locator();
+
+        public Locator()
+        {
+            Builder = new ContainerBuilder();
+        }
+
+        public ContainerBuilder Builder { get; private set; }
+
+        public void Build()
+        {
+            _container = Builder.Build();
+        }
+
+        public void Register<T>() where T : class
+        {
+            Builder.RegisterType<T>();
+        }
+
+        public void Register<T>(T instance) where T : class
+        {
+            Builder.RegisterInstance(instance);
+        }
+
+        public void Register<TInterface, TImplementation>() where TImplementation : TInterface
+        {
+            Builder.RegisterType<TImplementation>().As<TInterface>();
+        }
+
+        public T Resolve<T>()
+        {
+            if (null == _container)
+            {
+                throw new InvalidOperationException("Locator has not been built.");
+            }
+
+            return _container.Resolve<T>();
+        }
+
+        public object Resolve(Type type)
+        {
+            if (null == _container)
+            {
+                throw new InvalidOperationException("Locator has not been built.");
+            }
+
+            return _container.Resolve(type);
+        }
+    }
+}
---
+++ src/ContactlessEntry.WpfFront/ViewModels/ViewModelBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Input;
+
+namespace ContactlessEntry.WpfFront.ViewModels
+{
+    [PropertyChanged.AddINotifyPropertyChangedInterface]
+    public abstract class ViewModelBase
+    {
+        public bool IsBusy { get; set; }
+    }
+}
